4fb3c1bdb5276a4714ad2bd07ca67cd8d2ad2ebc
Tom Dawes <>
2021-03-17 13:51:20 +0000

    Add icons and start work on menu screen iOS

--- src/SkyDrop.iOS/Views/Menu/MenuView.cs
+++ src/SkyDrop.iOS/Views/Menu/MenuView.cs
@@ -17,15 +17,8 @@ namespace SkyDrop.iOS.Views.Menu
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
 
 
-        }
-
-        public override void DidReceiveMemoryWarning()
-        {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
         }
     }
 }
