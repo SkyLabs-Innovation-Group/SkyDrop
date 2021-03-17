f6c198c6b1346d5099c80e08ff162a3b18ed8920
Tom Dawes <>
2021-03-17 14:09:14 +0000

    Improve styling of main menu iOS

--- src/SkyDrop.iOS/Views/Menu/MenuView.cs
+++ src/SkyDrop.iOS/Views/Menu/MenuView.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using Acr.UserDialogs;
 using MvvmCross.Platforms.Ios.Presenters.Attributes;
 using MvvmCross.Platforms.Ios.Views;
+using SkyDrop.Core.Utility;
 using SkyDrop.Core.ViewModels.Main;
 using UIKit;
 
@@ -18,7 +20,20 @@ namespace SkyDrop.iOS.Views.Menu
         {
             base.ViewDidLoad();
 
+            //TODO: dark green navigation bar with title seems to be covered by another (grey) navigation bar
+            NavigationController.Title = "SkyDrop";
+            NavigationController.NavigationBar.BackgroundColor = Colors.PrimaryDark.ToNative();
 
+            View.BackgroundColor = Colors.DarkGrey.ToNative();
+            FileTransferButton.BackgroundColor = Colors.MidGrey.ToNative();
+            SettingsButton.BackgroundColor = Colors.MidGrey.ToNative();
+            FileTransferLabel.TextColor = Colors.LightGrey.ToNative();
+            SettingsLabel.TextColor = Colors.LightGrey.ToNative();
+            FileTransferIcon.TintColor = Colors.LightGrey.ToNative();
+            SettingsIcon.TintColor = Colors.LightGrey.ToNative();
+
+            FileTransferButton.Layer.CornerRadius = 8;
+            SettingsButton.Layer.CornerRadius = 8;
         }
     }
 }
--- src/SkyDrop.iOS/Views/Menu/MenuView.designer.cs
+++ src/SkyDrop.iOS/Views/Menu/MenuView.designer.cs
@@ -12,9 +12,55 @@ namespace SkyDrop.iOS.Views.Menu
 	[Register ("MenuView")]
 	partial class MenuView
 	{
+		[Outlet]
+		UIKit.UIView FileTransferButton { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView FileTransferIcon { get; set; }
+
+		[Outlet]
+		UIKit.UILabel FileTransferLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIView SettingsButton { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView SettingsIcon { get; set; }
+
+		[Outlet]
+		UIKit.UILabel SettingsLabel { get; set; }
 		
 		void ReleaseDesignerOutlets ()
 		{
+			if (FileTransferButton != null) {
+				FileTransferButton.Dispose ();
+				FileTransferButton = null;
+			}
+
+			if (SettingsButton != null) {
+				SettingsButton.Dispose ();
+				SettingsButton = null;
+			}
+
+			if (FileTransferLabel != null) {
+				FileTransferLabel.Dispose ();
+				FileTransferLabel = null;
+			}
+
+			if (SettingsLabel != null) {
+				SettingsLabel.Dispose ();
+				SettingsLabel = null;
+			}
+
+			if (FileTransferIcon != null) {
+				FileTransferIcon.Dispose ();
+				FileTransferIcon = null;
+			}
+
+			if (SettingsIcon != null) {
+				SettingsIcon.Dispose ();
+				SettingsIcon = null;
+			}
 		}
 	}
 }
