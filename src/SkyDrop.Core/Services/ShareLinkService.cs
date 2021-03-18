b6d9d157c0b58faa7a3ccf54efd7d4fa92c73953
Tom Dawes <>
2021-03-18 23:41:31 +0000

    Show toast when sharing

--- src/SkyDrop.Core/Services/ShareLinkService.cs
+++ src/SkyDrop.Core/Services/ShareLinkService.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Acr.UserDialogs;
 using Realms;
 using SkyDrop.Core.DataModels;
 using Xamarin.Essentials;
@@ -10,14 +11,19 @@ namespace SkyDrop.Core.Services
     public class ShareLinkService : IShareLinkService
     {
         private readonly ILog log;
+        private readonly IUserDialogs userDialogs;
 
-        public ShareLinkService(ILog log)
+        public ShareLinkService(ILog log,
+                                IUserDialogs userDialogs)
         {
             this.log = log;
+            this.userDialogs = userDialogs;
         }
 
         public async Task ShareLink(string skylink)
         {
+            userDialogs.Toast("Sharing");
+
             await Share.RequestAsync(new ShareTextRequest
             {
                 Text = skylink,
