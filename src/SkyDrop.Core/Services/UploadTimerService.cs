4eb6e80045c4b411be9688720494f5b6df419b7a
Jacob <jacob.hatwell@gmail.com>
2021-08-29 23:39:22 +0100

    Fix updating the timer text once per second

--- src/SkyDrop.Core/Services/UploadTimerService.cs
+++ src/SkyDrop.Core/Services/UploadTimerService.cs
@@ -16,10 +16,9 @@ namespace SkyDrop.Core.Services.Api
     {
         private readonly ILog log;
         private readonly IStorageService storageService;
-        private Stopwatch stopwatch;
+
+        public Stopwatch Stopwatch { get; set; }
         private Timer timer;
-        private TimeSpan estimatedUploadTime;
-        private long fileSizeBytes;
 
         public UploadTimerService(ILog log,
                                   IStorageService storageService)
@@ -28,90 +27,34 @@ namespace SkyDrop.Core.Services.Api
             this.storageService = storageService;
         }
 
-        public void AddReading(TimeSpan time, long fileSizeBytes)
-        {
-            var uploadRate = (fileSizeBytes * 8) / time.TotalSeconds; //in bits/second
-
-            //add the new value to the average
-            var currentAverage = storageService.GetAverageUploadRate();
-            var newDataPointCount = currentAverage.DataPointCount + 1;
-            var newAverage = new UploadAverage
-            {
-                DataPointCount = newDataPointCount,
-                Value = (currentAverage.Value * currentAverage.DataPointCount + uploadRate) / newDataPointCount
-            };
-
-            log.Trace($"New upload rate: {uploadRate}b/s");
-            log.Trace($"New average upload rate: {newAverage.Value}b/s");
-
-            //save new average
-            storageService.SetAverageUploadRate(newAverage);
-        }
-
-        public TimeSpan EstimateUploadTime(long fileSizeBytes)
-        {
-            long fileSizeBits = fileSizeBytes * 8;
-            var currentAverage = storageService.GetAverageUploadRate();
-            var averageUploadSpeed = currentAverage.Value;
-            if (averageUploadSpeed == 0)
-                averageUploadSpeed = 800_000; //default 800kb/s speed
-
-            var secondsPerBit = 1 / averageUploadSpeed;
-            var extraSeconds = 5; //let's add some time to play it safe (overestimate for user satisfaction)
-            var estimatedSeconds = fileSizeBits * secondsPerBit + extraSeconds;
-
-            return TimeSpan.FromSeconds(estimatedSeconds);
-        }
 
         public void StartUploadTimer(long fileSizeBytes, Action timerUpdateCallback)
         {
-            this.fileSizeBytes = fileSizeBytes;
-            estimatedUploadTime = EstimateUploadTime(fileSizeBytes);
-
-            stopwatch = new Stopwatch();
+            Stopwatch = new Stopwatch();
             timer = new Timer();
             timer.Elapsed += (s, e) => timerUpdateCallback();
             timer.Interval = 1000;
             timer.Enabled = true;
             timer.Start();
-            stopwatch.Start();
+            Stopwatch.Start();
             timerUpdateCallback();
         }
 
         public void StopUploadTimer()
         {
-            if (stopwatch == null) return;
+            if (Stopwatch == null) return;
 
-            if (stopwatch.IsRunning)
-            {
-                //save the upload time and file size to calculate average upload speed
-                AddReading(stopwatch.Elapsed, fileSizeBytes);
-            }
-
-            stopwatch.Stop();
+            Stopwatch.Stop();
             timer.Stop();
-        }
-
-        public (double UploadProgress, string UploadTime) GetUploadProgress()
-        {
-            var uploadTimerText = stopwatch.Elapsed.ToString(@"mm\:ss");
-            var uploadProgress = stopwatch.Elapsed.TotalSeconds / estimatedUploadTime.TotalSeconds;
-            log.Trace($"Upload Progress: {uploadProgress}, Upload Time: {uploadTimerText}");
-
-            return (uploadProgress, uploadTimerText);
         }
     }
 
     public interface IUploadTimerService
     {
-        void AddReading(TimeSpan time, long fileSizeBytes);
-
-        TimeSpan EstimateUploadTime(long fileSizeBytes);
+        Stopwatch Stopwatch { get; set; }
 
         void StartUploadTimer(long fileSizeBytes, Action timerUpdateCallback);
 
         void StopUploadTimer();
-
-        (double UploadProgress, string UploadTime) GetUploadProgress();
     }
 }
--- src/SkyDrop.Core/ViewModels/DropViewModel.cs
+++ src/SkyDrop.Core/ViewModels/DropViewModel.cs
@@ -29,7 +29,7 @@ namespace SkyDrop.Core.ViewModels.Main
         private readonly IFileSystemService fileSystemService;
         private readonly IBarcodeService barcodeService;
         private readonly IShareLinkService shareLinkService;
-        //private readonly IUploadTimerService uploadTimerService;
+        private readonly IUploadTimerService uploadTimerService;
 
         public IMvxCommand SendCommand { get; set; }
         public IMvxCommand ReceiveCommand { get; set; }
@@ -123,15 +123,16 @@ namespace SkyDrop.Core.ViewModels.Main
             IStorageService storageService,
             IBarcodeService barcodeService,
             IShareLinkService shareLinkService,
-            IUploadTimerService uploadTimerService,
             IUserDialogs userDialogs,
             IMvxNavigationService navigationService,
             IFileSystemService fileSystemService,
-            ILog log) : base(singletonService)
+            ILog log,
+            IUploadTimerService uploadTimerService) : base(singletonService)
         {
             Log = log;
             Title = "SkyDrop";
 
+            this.uploadTimerService = uploadTimerService;
             this.apiService = apiService;
             this.storageService = storageService;
             this.userDialogs = userDialogs;
@@ -139,7 +140,6 @@ namespace SkyDrop.Core.ViewModels.Main
             this.fileSystemService = fileSystemService;
             this.barcodeService = barcodeService;
             this.shareLinkService = shareLinkService;
-            //this.uploadTimerService = uploadTimerService;
 
             SendCommand = new MvxAsyncCommand(async () => await SendButtonTapped());
             ReceiveCommand = new MvxAsyncCommand(async () => await ReceiveFile());
@@ -488,36 +488,17 @@ namespace SkyDrop.Core.ViewModels.Main
 
         private void StartUploadTimer(long fileSizeBytes)
         {
-            //uploadTimerService.StartUploadTimer(fileSizeBytes, UpdateUploadProgress);
+            uploadTimerService.StartUploadTimer(fileSizeBytes, UpdateUploadProgress);
         }
 
         private void StopUploadTimer()
         {
-            //fill progress bar
-            UploadProgress = 1;
-            UploadTimerText = "100%";
-
-            //uploadTimerService.StopUploadTimer();
+            uploadTimerService.StopUploadTimer();
         }
 
         private void UpdateUploadProgress()
         {
-            //var (newUploadProgress, newUploadTimerText) = uploadTimerService.GetUploadProgress();
-
-            if (UploadProgress >= 1)
-                return;
-
-            //scale the progress so it fits within 85% of the bar
-            var maxProgress = 0.85;
-            //newUploadProgress *= maxProgress;
-            //if (newUploadProgress > maxProgress)
-            //    newUploadProgress = maxProgress;
-
-            //UploadProgress = newUploadProgress;
-            //UploadTimerText = newUploadTimerText;
-            
-            //if (UploadNotificationsEnabled)
-            //    UpdateNotificationProgressCommand?.Execute(newUploadProgress);
+            UploadTimerText = uploadTimerService.Stopwatch.Elapsed.ToString(@"mm\:ss");
         }
 
         private void UpdateFileSize()
