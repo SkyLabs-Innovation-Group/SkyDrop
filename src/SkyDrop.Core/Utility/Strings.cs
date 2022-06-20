5e6a43b3b1955ad7cb2caebf9af163b4b215739f
Tom <toxictomdude@gmail.com>
2022-06-20 17:29:56 +0100

    Update string

--- src/SkyDrop.Core/Utility/Strings.cs
+++ src/SkyDrop.Core/Utility/Strings.cs
@@ -6,7 +6,7 @@ namespace SkyDrop.Core.Utility
         public const string SslPrompt = "Failed to verify SSL certificate. If you trust this network, try disabling the Require Secure SSL Connection setting.";
 
         //for iOS multiple image picker
-        public static string NoPhotoAccessMessage = "You have not granted this app access to your photos. Please enable access in iOS Settings.";
+        public static string NoPhotoAccessMessage = "Please give SkyDrop permission to access Photos in iOS Settings.";
         public static string NoPhotoAccessTitle = "No photos access";
         public static string Settings = "Settings";
         public static string Cancel = "Cancel";
