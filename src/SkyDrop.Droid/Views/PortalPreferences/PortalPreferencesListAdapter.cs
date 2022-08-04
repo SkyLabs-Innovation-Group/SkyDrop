bbd9792368de1481b1609e797ec7977bea333859
Jacob <jacob.hatwell@gmail.com>
2022-08-04 13:40:30 +0100

    Add portal preferences drag calculation and reordering logic

---
+++ src/SkyDrop.Droid/Views/PortalPreferences/PortalPreferencesListAdapter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.Support.V7.Widget;
+using Android.Views;
+using MvvmCross.Droid.Support.V7.RecyclerView;
+using MvvmCross.Platforms.Android.Binding.BindingContext;
+
+namespace SkyDrop.Droid.Views.PortalPreferences
+{
+    internal class PortalPreferencesListAdapter : MvxRecyclerAdapter
+    {
+        private readonly PortalPreferencesListView view;
+
+        public PortalPreferencesListAdapter(PortalPreferencesListView view, IMvxAndroidBindingContext bindingContext) : base(bindingContext)
+        {
+            this.view = view;
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            var portalViewHolder = (PortalViewHolder) holder;
+            portalViewHolder.Bind(this);
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            var itemView = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.item_skynet_portal, parent, false);
+            return new PortalViewHolder(itemView, BindingContext);
+        }
+
+        internal void ReorderPortals(int position, int newPosition) => view.ReorderPortals(position, newPosition);
+    }
+}
+
