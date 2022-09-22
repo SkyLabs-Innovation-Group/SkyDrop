2715a76ce3a10201b5383100de61bb2ab887fc80
Tom Dawes <tomdawes@hotmail.co.uk>
2022-09-22 22:51:13 +0100

    Implement pairing error overlay Android

--- src/SkyDrop.Droid/Bindings/PairingOverlayBinding.cs
+++ src/SkyDrop.Droid/Bindings/PairingOverlayBinding.cs
@@ -1,9 +1,12 @@
 using System.Drawing;
 using Acr.UserDialogs;
 using Android.Views;
+using Android.Widget;
 using AndroidX.CardView.Widget;
 using MvvmCross.Binding;
 using MvvmCross.Binding.Bindings.Target;
+using SkyDrop.Core.Utility;
+using SkyDrop.Droid.Helper;
 using static SkyDrop.Core.Services.EncryptionService;
 
 namespace SkyDrop.Droid.Bindings
@@ -20,17 +23,24 @@ namespace SkyDrop.Droid.Bindings
 
         protected override void SetValue(AddContactResult value)
         {
+            var checkIcon = Target.FindViewById<ImageView>(Resource.Id.checkIcon);
             switch(value)
             {
                 case AddContactResult.Default:
+                    Target.Visibility = ViewStates.Gone;
+                    break;
                 case AddContactResult.InvalidKey:
                 case AddContactResult.WrongDevice:
-                    Target.Visibility = ViewStates.Gone;
+                    Target.Visibility = ViewStates.Visible;
+                    Target.SetBackgroundColor(Colors.Red.ToNative());
+                    checkIcon.SetLocalImage("ic_error");
                     break;
                 case AddContactResult.DevicesPaired:
                 case AddContactResult.ContactAdded:
                 case AddContactResult.AlreadyExists:
                     Target.Visibility = ViewStates.Visible;
+                    Target.SetBackgroundColor(Colors.Primary.ToNative());
+                    checkIcon.SetLocalImage("ic_check");
                     break;
             }
         }
