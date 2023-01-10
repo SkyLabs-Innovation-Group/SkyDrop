7c8f60b17e5a60a8e8c489d840c00690a461e2f5
Jacob <jacob.hatwell@gmail.com>
2023-01-10 02:32:34 +0000

    Update RetryHandler iOS - still need to fix cancel button

--- src/SkyDrop.iOS/Common/RetryHandler.cs
+++ src/SkyDrop.iOS/Common/RetryHandler.cs
@@ -11,28 +11,39 @@ namespace SkyDrop.iOS.Common
     public class RetryHandler : NSUrlSessionHandler
     {
         private ILog _log;
-        private ILog log => (_log ??= Mvx.IoCProvider.Resolve<ILog>()); 
+        private ILog log => (_log ??= Mvx.IoCProvider.Resolve<ILog>());
 
         // Strongly consider limiting the number of retries - "retry forever" is
         // probably not the most user friendly way you could respond to "the
         // network cable got pulled out."
         private const int MaxRetries = 5;
 
-        public RetryHandler(NSUrlSessionConfiguration innerHandler)
-            : base(innerHandler)
+        public RetryHandler()
+            : base()
         { }
 
         protected override async Task<HttpResponseMessage> SendAsync(
             HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
+            log.Trace("RetryHandler Sending request ");
+            log.Trace(request.ToString());
             HttpResponseMessage response = null;
             for (int i = 0; i < MaxRetries; i++)
             {
-                if (i > 0)
-                    log.Error("Retrying upload: try number " + i);
-                response = await base.SendAsync(request, cancellationToken);
-                if (response.IsSuccessStatusCode)
+                log.Trace("Trying upload: try number " + i);
+
+                try
+                {
+                    response = await base.SendAsync(request, CancellationToken.None);
+                }
+                catch (Exception ex)
+                {
+                    log.Error("Error trying request try number " + i);
+                    log.Exception(ex);
+                }
+
+                if (response?.IsSuccessStatusCode ?? false)
                 {
                     return response;
                 }
