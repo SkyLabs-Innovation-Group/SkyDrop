fea09cc936b7fe98e607307d4ca21465cec65835
Jacob <jacob.hatwell@gmail.com>
2023-01-27 22:56:55 +0000

    Add unhandled exception reporting

--- src/SkyDrop.Droid/MainApplication.cs
+++ src/SkyDrop.Droid/MainApplication.cs
@@ -4,6 +4,7 @@ using Android.Runtime;
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
+using MvvmCross;
 using MvvmCross.Platforms.Android.Views;
 using SkyDrop.Core;
 using Xamarin.Essentials;
@@ -20,12 +21,23 @@ namespace SkyDrop.Droid
     {
         public const string AndroidAppCenterApiKey = "a4cdd96b-271f-4337-a1a6-57801ea8fd9c";
 
+        private ILog _log;
+        protected ILog log => (_log ??= Mvx.IoCProvider.Resolve<ILog>());
+
         public MainApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
         {
+            AndroidEnvironment.UnhandledExceptionRaiser += AndroidEnvironmentOnUnhandledExceptionRaiser;
+
             Platform.Init(this);
 
             AppCenter.Start(AndroidAppCenterApiKey,
                 typeof(Analytics), typeof(Crashes));
+        }
+
+        private void AndroidEnvironmentOnUnhandledExceptionRaiser(object sender, RaiseThrowableEventArgs e)
+        {
+            log.Error("Unhandled exception: ");
+            log.Exception(e.Exception, unhandled: true);
         }
     }
 }
--- src/SkyDrop.iOS/AppDelegate.cs
+++ src/SkyDrop.iOS/AppDelegate.cs
@@ -1,7 +1,10 @@
-﻿using Foundation;
+﻿using System;
+using System.Threading.Tasks;
+using Foundation;
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
+using MvvmCross;
 using MvvmCross.Platforms.Ios.Core;
 using SkyDrop.Core;
 using UIKit;
@@ -12,9 +15,15 @@ namespace SkyDrop.iOS
     [Register(nameof(AppDelegate))]
     public class AppDelegate : MvxApplicationDelegate<Setup, App>
     {
+        private ILog _log;
+        protected ILog log => (_log ??= Mvx.IoCProvider.Resolve<ILog>());
+
         public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
         {
             var val = base.FinishedLaunching(application, launchOptions);
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
 
             UNUserNotificationCenter.Current.RequestAuthorizationAsync(UNAuthorizationOptions.Announcement);
             UIApplication.SharedApplication.ApplicationIconBadgeNumber = 0;
@@ -23,6 +32,22 @@ namespace SkyDrop.iOS
                 typeof(Analytics), typeof(Crashes));
 
             return val;
+        }
+
+        private void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            LogException(e.Exception, nameof(TaskSchedulerOnUnobservedTaskException));
+        }
+
+        private void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            log.Error(e.ExceptionObject.ToString());
+        }
+
+        private void LogException(System.Exception ex, string errorMessage = "")
+        {
+            log.Error(errorMessage);
+            log.Exception(ex, unhandled: true);
         }
     }
 }
